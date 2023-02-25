b7b862945a275e159e04015f87a0bdd53a8f41bb
Jaden C <jc900@duke.edu>
2023-02-25 14:31:32 +0800

    editing date list and setting up to pull from/push to database

--- website/arrivaldate_returnlist.py
+++ website/arrivaldate_returnlist.py
@@ -6,6 +6,10 @@ def return_deadlines():
     year = int(input('Year: '))
     month = int(input('Month: '))
     day = int(input('Day: ')) 
+    print() 
+
+    today = datetime.now()
+    print("Today's Date:", today, "\n")
 
     if not 0 < month < 13:
        print('enter a valid month')
@@ -15,6 +19,9 @@ def return_deadlines():
        raise Exception(f'{day} is not a valid date in {month}') 
 
     arrivalDate = datetime(year, month, day)
+
+    # submit health insurance form
+    health_insurance = arrivalDate - timedelta(days = 3)
 
     # calculating the deadline for 24hr Form
     deadline24hr = arrivalDate + timedelta(days = 1)
@@ -37,25 +44,30 @@ def return_deadlines():
     bankdate = arrivalDate + timedelta(days = 3)
 
     ### Maybe attempt table / calendar output ??? 
-    print(f'Your Arrival Date: {year}-{month}-{day}')
+    print(f'Your Arrival Date: {year}-{month}-{day}\n')
+
+    print("Pre-Arrival to-dos:\n")
     
+    print("Submit Health Insurance Form (3 days before arrival):\n", \
+          health_insurance)
+
     print('\nNon-negotiable Deadlines:\n')
 
     print("Submit 24 hour form application by the end of the day", deadline24hr)
-    print("You will need this form to apply for your residence permit")
+    print("You will need this form to apply for your residence permit\n")
 
-    print("Health Checkup Appointment by", health_check_deadline)
+    print("Health Checkup Appointment by", health_check_deadline,"\n")
 
-    print("Last Day for Residence Permit Appointment: ", res_permit_deadline)
+    print("Last Day for Residence Permit Appointment: ", res_permit_deadline,"\n")
 
     print("\nSuggested Deadlines and Due-Dates:\n")
 
     print("Try to obtain your SIM card by", simdate)
-    print("You will need a Chinese phone number to open a bank account")
+    print("You will need a Chinese phone number to open a bank account","\n")
 
     print("Try to open your bank account by", bankdate)
     print("You will need a bank account to use Wechat Pay and in some instances," \
-        "to use Alipay.")
+        "to use Alipay.\n")
 
     print("try to schedule your health appointment on", health_check_date)
     print("new appointments are added on Thursday each week")
@@ -63,3 +75,5 @@ def return_deadlines():
     print("This is the ISS recommended timeline")
 
 return_deadlines()
+
+# push[userID, 'bankdate', bankdate]
--- website/calendar.py
+++ website/calendar.py
@@ -1,6 +1,12 @@
-import calendar
+from flask import Flask, render_template
+from arrivaldate_returnlist import *
 
-yy = 2023
-mm = 1
+app = Flask(__name__)
 
-print(calendar.month(yy,mm))
+@app.route('/')
+def index():
+    marked_dates = [arrivalDate, deadline24hr, healthcheck]  # list of dates to be marked
+    return render_template('calendar.html', marked_dates=marked_dates)
+
+if __name__ == '__main__':
+    app.run(debug=True)
