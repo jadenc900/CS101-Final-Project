b616cd8447e951ed29b1c102892cf62c8edfbee3
kykysuperfly23 <kyaira.boughton@gmail.com>
2023-02-25 16:37:00 +0800

    jk try now.

--- website/views.py
+++ website/views.py
@@ -24,14 +24,14 @@ def home():
     return render_template("mainpage.html", user=current_user)
 
 
-@views.route('/delete-note', methods=['POST'])
-def delete_note():  
-    note = json.loads(request.data) # this function expects a JSON from the INDEX.js file 
-    noteId = note['noteId']
-    note = Note.query.get(noteId)
-    if note:
-        if note.user_id == current_user.id:
-            db.session.delete(note)
-            db.session.commit()
-
-    return jsonify({})
+#@views.route('/delete-note', methods=['POST'])
+#def delete_note():  
+##    note = json.loads(request.data) # this function expects a JSON from the INDEX.js file 
+#    noteId = note['noteId']
+#    note = Note.query.get(noteId)
+#    if note:
+#        if note.user_id == current_user.id:
+# #          db.session.delete(note)
+#            db.session.commit()
+#
+#    return jsonify({})
