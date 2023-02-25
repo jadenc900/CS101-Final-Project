f71131d92b4fc2d7835aff60adfe4cf954f180f6
Jaden C <jc900@duke.edu>
2023-02-25 14:51:27 +0800

    reformatted dates and converted deadines to strings

--- website/arrivaldate_returnlist.py
+++ website/arrivaldate_returnlist.py
@@ -2,6 +2,7 @@ from datetime import *
 from logging.config import stopListening
 
 def return_deadlines():
+    # this will be changed to take from the database
     print("Enter Date/Intended Date of Arrival (YYYY-MM-DD):\n\n")
     year = int(input('Year: '))
     month = int(input('Month: '))
@@ -9,6 +10,7 @@ def return_deadlines():
     print() 
 
     today = datetime.now()
+    today = today.strftime("%Y-%m-%d")
     print("Today's Date:", today, "\n")
 
     if not 0 < month < 13:
@@ -22,26 +24,36 @@ def return_deadlines():
 
     # submit health insurance form
     health_insurance = arrivalDate - timedelta(days = 3)
+    health_insurance = health_insurance.strftime("%Y-%m-%d")
 
     # calculating the deadline for 24hr Form
     deadline24hr = arrivalDate + timedelta(days = 1)
+    deadline24hr = deadline24hr.strftime("%Y-%m-%d")
 
     # calulating the deadline for Residence Permit Appointment
     permit_date = datetime(year, month+1, day)
+    permit_date = permit_date.strftime("%Y-%m-%d")
+
+    permit_date = datetime.strptime(permit_date,"%Y-%m-%d")
 
     res_permit_deadline = permit_date - timedelta(days = 1)
+    res_permit_deadline = res_permit_deadline.strftime("%Y-%m-%d")
 
     # Health Check recommended day 
     health_check_date = arrivalDate + timedelta(days = 6)
+    health_check_date = health_check_date.strftime("%Y-%m-%d")
 
     # Health Checkup suggested last day
     health_check_deadline = permit_date - timedelta(days = 7)
+    health_check_deadline = health_check_deadline.strftime("%Y-%m-%d")
 
     # SIM Card suggested timeline
     simdate = arrivalDate + timedelta(days = 2)
+    simdate = simdate.strftime("%Y-%m-%d")
 
     # Bank Account suggested
     bankdate = arrivalDate + timedelta(days = 3)
+    bankdate = bankdate.strftime("%Y-%m-%d")
 
     ### Maybe attempt table / calendar output ??? 
     print(f'Your Arrival Date: {year}-{month}-{day}\n')
@@ -69,10 +81,10 @@ def return_deadlines():
     print("You will need a bank account to use Wechat Pay and in some instances," \
         "to use Alipay.\n")
 
-    print("try to schedule your health appointment on", health_check_date)
-    print("new appointments are added on Thursday each week")
+    print("try to schedule your health appointment on", health_check_date,". \n")
+    print("New appointments are added on Thursday each week")
 
-    print("This is the ISS recommended timeline")
+    print("\n This is the ISS recommended timeline \n")
 
 return_deadlines()
 
